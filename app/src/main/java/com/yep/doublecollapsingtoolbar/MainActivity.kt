b4e6714200859062279e33446ec43c9e86d00f93
Yurii Poudanien <mcpchip@gmail.com>
2022-08-23 23:24:24 +0300

    - added Last In First Out ordering for collapsing

--- app/src/main/java/com/yep/doublecollapsingtoolbar/MainActivity.kt
+++ app/src/main/java/com/yep/doublecollapsingtoolbar/MainActivity.kt
@@ -2,7 +2,10 @@ package com.yep.doublecollapsingtoolbar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.LayoutParams.*
 import com.google.android.material.tabs.TabLayout
 import com.yep.doublecollapsingtoolbar.databinding.ActivityMainBinding
 
@@ -21,12 +24,45 @@ class MainActivity : AppCompatActivity() {
         }
         binding.mainTabLayoutFirst.setupTabLayout()
         binding.mainTabLayoutSecond.setupTabLayout()
+        binding.mainImageCollapsingToolbarFirst.tag = false
+        binding.appbarLayout.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
+            override fun onStateChanged(appBarLayout: AppBarLayout?, state: State?) {
+                when (state) {
+                    State.COLLAPSED -> {
+                        setBlockLayout(false)
+                    }
+                    else -> {}
+                }
+            }
+        }
+        )
+        binding.appbarLayout2.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
+            override fun onStateChanged(appBarLayout: AppBarLayout?, state: State?) {
+                when (state) {
+                    State.EXPANDED -> {
+                        setBlockLayout(true)
+                    }
+                    else -> {}
+                }
+            }
+        }
+        )
     }
 
     private fun TabLayout.setupTabLayout() {
         if (this.tabCount == 0) {
             this.addTab(this.newTab().setText("Tab 1"))
             this.addTab(this.newTab().setText("Tab 2"))
+        }
+    }
+
+    fun setBlockLayout(state: Boolean) {
+        binding.mainImageCollapsingToolbarFirst.updateLayoutParams<AppBarLayout.LayoutParams> {
+            scrollFlags = if (state) {
+                SCROLL_FLAG_ENTER_ALWAYS or SCROLL_FLAG_SCROLL or SCROLL_FLAG_SNAP
+            } else {
+                SCROLL_FLAG_EXIT_UNTIL_COLLAPSED or SCROLL_FLAG_SCROLL or SCROLL_FLAG_SNAP
+            }
         }
     }
 }
