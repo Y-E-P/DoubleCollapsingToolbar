2611d2eaca1894959f1fbf254149f7dd6315cd16
Yurii Poudanien <mcpchip@gmail.com>
2022-08-23 22:53:34 +0300

    - grouped all together

--- app/src/main/java/com/yep/doublecollapsingtoolbar/MainActivity.kt
+++ app/src/main/java/com/yep/doublecollapsingtoolbar/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.yep.doublecollapsingtoolbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayout
+import com.yep.doublecollapsingtoolbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (binding.mainRecyclerView.adapter == null) {
+            binding.mainRecyclerView.adapter = RecyclerAdapter()
+            binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+            binding.mainRecyclerView.isNestedScrollingEnabled = true
+        }
+        binding.mainTabLayoutFirst.setupTabLayout()
+        binding.mainTabLayoutSecond.setupTabLayout()
+    }
+
+    private fun TabLayout.setupTabLayout() {
+        if (this.tabCount == 0) {
+            this.addTab(this.newTab().setText("Tab 1"))
+            this.addTab(this.newTab().setText("Tab 2"))
+        }
     }
 }
